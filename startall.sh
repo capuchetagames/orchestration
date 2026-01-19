d918cbd0e047de5cdc76118423be5a14e1d64353
Diogo Carlomagno <diogocarlomagno@gmail.com>
2026-01-19 18:43:56 -0300

    file name changes in docker-compose file

--- startall.sh
+++ startall.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 echo "Subindo Users Services..."
-docker-compose -f ../usersapi/compose.yaml up -d
+docker-compose -f ../usersapi/docker-compose.yaml up -d
 
 echo "Subindo Catalog Services..."
-docker-compose -f ../catalogapi/compose.yaml up -d
+docker-compose -f ../catalogapi/docker-compose.yaml up -d
 
 echo "Subindo Payments Services..."
-docker-compose -f ../paymentsapi/compose.yaml up -d
+docker-compose -f ../paymentsapi/docker-compose.yaml up -d
 
 echo "Subindo Notifications Services..."
-docker-compose -f ../notificationsapi/compose.yaml up -d
+docker-compose -f ../notificationsapi/docker-compose.yaml up -d
 
 
 echo "Tudo rodando! 🚀"
